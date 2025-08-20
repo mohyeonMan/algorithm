4c673e244739d2964b4b70dc764a97ee41a83d96
mohyeonMan <qkrwlgns0510@gmail.com>
2025-08-20 22:21:10 +0900

    feat : 25083

---
+++ src/questions/bakjoon/25083.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("         ,r'\"7\n" + //
+                        "r`-_   ,'  ,/\n" + //
+                        " \\. \". L_r'\n" + //
+                        "   `~\\/\n" + //
+                        "      |\n" + //
+                        "      |");
+
+    }
+}
