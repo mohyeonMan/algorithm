d3d5eec82362eebf71bf170d2831f5f6e16bc481
박지훈 <qkrwlgns0510@gmail.com>
2025-09-30 08:32:22 +0900

    feat : 15894

---
+++ src/questions/bakjoon/15894.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
