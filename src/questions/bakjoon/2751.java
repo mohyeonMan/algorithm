d12b893ca05333b7782264c5d764b886d604f7ff
mohyeonMan <qkrwlgns0510@gmail.com>
2025-10-13 21:20:07 +0900

    feat : 2751

---
+++ src/questions/bakjoon/2751.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        for (int n : arr) sb.append(n).append('\n');
+
+        System.out.print(sb);
+    }
+}
